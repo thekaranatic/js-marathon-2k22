64734ca1db3c7e2125c6d7f59069510a3a5a5b6d
Karan Kakati <karanpkofficial@gmail.com>
2022-06-20 23:53:17 +0530

    Create hellokk.js

---
+++ src/101-basics/hellokk.js
@@ -0,0 +1,28 @@
+// console.log("hello kk")
+
+// variable declaration
+let username = "KK"
+username = "notion"
+
+let userMobile = 1234567890
+let isLoggedIn = true
+
+let testMeLet
+// var testMeVar : Not a recommneded way to declare variables
+
+// console.log(testMeLet);
+// console.log(testMeVar);
+
+
+// case sensitivity 
+var ONLINE 
+
+// var 1_online : doesnt work
+
+// modify to by beginning with $
+var $1_online
+
+// camel casing is a good practice
+var yourMobileNumber
+
+
